89d3c0821f5e5c1e3998ede405d9524d560e9ef2
Hemanth <hemanthjabalpuri@gmail.com>
2021-05-21 15:58:26 +0530

    Update for OFox

--- build.sh
+++ build.sh
@@ -6,21 +6,30 @@ DEVICE=RMX2185
 DT_LINK="https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185"
 DT_PATH=device/realme/$DEVICE
 TARGET=recoveryimage
-OUTFILE=TWRP-${DEVICE}.zip
+OUTFILE=OFox-${DEVICE}.zip
 
 echo " ===+++ Setting up Build Environment +++==="
 mkdir -p /tmp/recovery
 cd /tmp/recovery
-apt install openssh-server -y
+git clone https://gitlab.com/OrangeFox/misc/scripts
+cd scripts
+sudo bash setup/android_build_env.sh
+sudo bash setup/install_android_sdk.sh
 
 echo " ===+++ Syncing Recovery Sources +++==="
-repo init --depth=1 -u $MANIFEST -g default,-device,-mips,-darwin,-notdefault 
-repo sync -j$(nproc --all)
-git clone $DT_LINK $DT_PATH
+git clone https://gitlab.com/OrangeFox/sync.git
+cd sync
+echo 'y' | ./get_fox_10.sh fox_10.0
+cd fox_10.0
+git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
 rm -rf out
-. build/envsetup.sh && lunch omni_${DEVICE}-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka $TARGET
+source build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true 
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1 
+export LC_ALL="C"
+lunch omni_${DEVICE}-eng && mka $TARGET
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
