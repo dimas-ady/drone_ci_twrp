b3ded66cad7af709571891ccccfb7b53822e11a3
Hemanth <hemanthjabalpuri@gmail.com>
2021-05-23 19:37:27 +0530

    Misc changes

--- build.sh
+++ build.sh
@@ -3,9 +3,8 @@
 
 MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
 DEVICE=RMX2185
-DT_LINK="https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185"
+DT_LINK="https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185 -b android-10.0"
 DT_PATH=device/realme/$DEVICE
-TARGET=recoveryimage
 OUTFILE=TWRP-${DEVICE}.zip
 
 echo " ===+++ Setting up Build Environment +++==="
@@ -16,20 +15,24 @@ apt install openssh-server -y
 echo " ===+++ Syncing Recovery Sources +++==="
 repo init --depth=1 -u $MANIFEST -g default,-device,-mips,-darwin,-notdefault 
 repo sync -j$(nproc --all)
-git clone $DT_LINK $DT_PATH
+git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
 rm -rf out
-. build/envsetup.sh && lunch omni_${DEVICE}-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka $TARGET
+source build/envsetup.sh
+lunch omni_${DEVICE}-eng
+export ALLOW_MISSING_DEPENDENCIES=true
+export LC_ALL="C"
+mka recoveryimage
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
 cd out/target/product/$DEVICE
 zip -r9 $OUTFILE recovery.img
-curl -sL https://git.io/file-transfer | sh 
-./transfer wet $OUTFILE
+curl -T ./$OUTFILE https://oshi.at
 if [ $? != 0 ]; then
-  curl -T ./$OUTFILE https://oshi.at
+  curl -sL https://git.io/file-transfer | sh 
+  ./transfer wet $OUTFILE
   if [ $? != 0 ]; then
     #https://github.com/dutchcoders/transfer.sh/issues/116
     curl --upload-file ./$OUTFILE http://transfer.sh/$OUTFILE
