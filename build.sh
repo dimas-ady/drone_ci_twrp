02e10695e9e344260762bbe0d32730424e53b13d
Hemanth <hemanthjabalpuri@gmail.com>
2021-05-25 09:44:40 +0530

    Update twrp-test

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
 DEVICE=RMX2185
-DT_LINK="https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185 -b android-10.0"
+DT_LINK="https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185 -b twrp-test"
 DT_PATH=device/realme/$DEVICE
 OUTFILE=TWRP-${DEVICE}.zip
 
@@ -28,12 +28,4 @@ lunch omni_${DEVICE}-eng && mka recoveryimage
 echo " ===+++ Uploading Recovery +++==="
 cd out/target/product/$DEVICE
 zip -r9 $OUTFILE recovery.img
-curl -T ./$OUTFILE https://oshi.at
-if [ $? != 0 ]; then
-  curl -sL https://git.io/file-transfer | sh 
-  ./transfer wet $OUTFILE
-  if [ $? != 0 ]; then
-    #https://github.com/dutchcoders/transfer.sh/issues/116
-    curl --upload-file ./$OUTFILE http://transfer.sh/$OUTFILE
-  fi
-fi
+curl -F "file=@${OUTFILE}" https://file.io
