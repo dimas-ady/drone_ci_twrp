0ead34cd410216158f296a92f811153225c3127c
Hemanth <hemanthjabalpuri@gmail.com>
2021-05-26 21:25:14 +0530

    Update checks

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Just a basic script U can improvise lateron asper ur need xD 
 
+abort() { echo "$1"; exit 1 }
+
 MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
 DEVICE=RMX2185
-DT_LINK="https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185 -b twrp-test"
+DT_LINK="https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185 -b android-10.0"
 DT_PATH=device/realme/$DEVICE
 OUTFILE=TWRP-${DEVICE}.zip
 
@@ -20,23 +22,19 @@ git clone --depth=1 $DT_LINK $DT_PATH
 echo " ===+++ Building Recovery +++==="
 rm -rf out
 source build/envsetup.sh
+echo " source build/envsetup.sh done"
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
-lunch omni_${DEVICE}-eng && mka recoveryimage
+lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
+echo " lunch omni_${DEVICE}-eng done"
+mka recoveryimage || abort " mka failed with exit status $?"
+echo " mka recoveryimage done"
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
 cd out/target/product/$DEVICE
 zip -r9 $OUTFILE recovery.img
 
-transferFile() {
-  echo " Uploading $1"
-  curl -F "file=@${1}" https://file.io
-  sleep 3
-  curl -T $1 https://oshi.at
-  sleep 3
-  curl --upload-file $1 http://transfer.sh/$1
-}
-
-transferFile $OUTFILE
-sleep 10
+curl -T $OUTFILE https://oshi.at
+#curl -F "file=@${OUTFILE}" https://file.io
+#curl --upload-file $OUTFILE http://transfer.sh/
