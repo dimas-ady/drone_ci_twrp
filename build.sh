f8685a93ae686953a816384c889f6159f9deb5a6
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-02 15:49:45 +0530

    Create build.sh

--- build.sh
+++ build.sh
@@ -2,22 +2,25 @@
 
 mkdir ~/work
 
-echo "Cloning kernel sources"
+echo "===+++ Cloning kernel sources +++==="
 cd ~/work
 git clone --depth=1 https://github.com/HemanthJabalpuri/android_kernel_realme_mt6765 -b android-10.0 kernel
 
-echo "Downloading toolchain"
+echo "===+++ Downloading toolchain +++==="
 mkdir ~/work/toolchain && cd ~/work/toolchain
-wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-10.0.0_r47/clang-r353983c.tar.gz -O clang-r353983c.tar.gz
-tar zxvf clang-r353983c.tar.gz
-rm -f clang-r353983c.tar.gz
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/ee5ad7f5229892ff06b476e5b5a11ca1f39bf3a9/clang-r365631c.tar.gz -O clang.tar.gz
+tar zxvf clang.tar.gz
+rm -f clang.tar.gz
 
-echo "Building kernel"
+echo "===+++ Building kernel +++==="
 cd ~/work/kernel
+echo "Start of ls output"
+ls
+echo "End of ls output"
 mkdir out
 make O=out ARCH=arm64 RMX2185_defconfig
 
-echo "Compiling..."
+echo "===+++ Compiling... +++==="
 PATH=$HOME/work/toolchain/bin:$PATH \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
