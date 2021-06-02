9cdb5bdb0737945e366c7a43454c22b710ee7bf9
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-02 15:14:37 +0530

    Create build.sh

--- build.sh
+++ build.sh
@@ -2,22 +2,23 @@
 
 mkdir ~/work
 
-# Clone kernel sources
+echo "Cloning kernel sources"
 mkdir ~/work/kernel && cd ~/work/kernel
 git clone --depth=1 https://github.com/HemanthJabalpuri/android_kernel_realme_mt6765 -b android-10.0
 
-# Download toolchain
+echo "Downloading toolchain"
 mkdir ~/work/toolchain && cd ~/work/toolchain
 wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-10.0.0_r47/clang-r353983c.tar.gz -O clang-r353983c.tar.gz
 tar zxvf clang-r353983c.tar.gz
 rm -f clang-r353983c.tar.gz
 
-# Build kernel
+echo "Building kernel"
 cd ~/work/kernel
 mkdir out
-export ARCH=arm64
+export PATH=$HOME/work/toolchain/bin:$PATH
 make O=out ARCH=arm64 RMX2185_defconfig
-export PATH=$HOME/toolchain/bin:$PATH
+
+echo "Compiling..."
 make O=out \
      ARCH=arm64 \
      CC=clang \
