00ba111f715e13b1c18845df6bde29e7b575e6d0
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-06 14:17:40 +0530

    Create build.sh

--- build.sh
+++ build.sh
@@ -1,31 +1,3 @@
 #!/bin/sh
 
-mkdir ~/work
-
-echo "===+++ Cloning kernel sources +++==="
-cd ~/work
-git clone --depth=1 https://github.com/HemanthJabalpuri/android_kernel_realme_mt6765 -b android-10.0 kernel
-
-echo "===+++ Downloading toolchain +++==="
-mkdir toolchain
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
-
-echo "===+++ Building kernel +++==="
-cd ~/work/kernel
-mkdir out
-export LC_ALL=C && export USE_CCACHE=1
-ccache -M 100G
-make O=out ARCH=arm64 RMX2185_defconfig
-
-echo "===+++ Compiling... +++==="
-PATH="$HOME/work/toolchain/clang/clang-r353983c/bin:${PATH}:$HOME/work/toolchain/los-4.9-32/bin:${PATH}:$HOME/work/toolchain/los-4.9-64/bin:${PATH}" \
-make O=out \
-     ARCH=arm64 \
-     CC="ccache clang" \
-     CLANG_TRIPLE=aarch64-linux-gnu- \
-     CROSS_COMPILE="$HOME/work/toolchain/los-4.9-64/bin/aarch64-linux-android-" \
-     CROSS_COMPILE_ARM32="$HOME/work/toolchain/los-4.9-32/bin/arm-linux-androideabi-" \
-     CONFIG_NO_ERROR_ON_MISMATCH=y \
-     -j8
+df -h
