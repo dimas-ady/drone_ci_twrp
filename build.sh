0c64915aebf7a9ab66b42e9d7639633a462424e3
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-06 14:31:17 +0530

    Create build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-df -h
+git clone https://github.com/akhilnarang/scripts.git
+cd scripts
+bash setup/android_build_env.sh
+
+cd
+mkdir work && cd work
+repo init -u git://github.com/LineageOS/android.git -b lineage-18.1
+repo sync --force-sync -c --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc)
+
+git clone --depth=1 https://github.com/HemanthJabalpuri/android_device_realme_RMX2185 -b initial device/realme/RMX2185
+
+source build/envsetup.sh
+lunch lineage_RMX2185-eng
+mka bacon
