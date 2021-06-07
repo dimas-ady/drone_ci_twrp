050a5ad6dd074b9ead9e17a5e20d2061f775e492
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-07 16:44:03 +0530

    Update clang and misc

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ git clone --depth=1 https://github.com/HemanthJabalpuri/android_kernel_realme_mt
 
 echo "===+++ Downloading toolchain +++==="
 mkdir toolchain && cd toolchain
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
+git clone --depth=1 https://github.com/techyminati/android_prebuilts_clang_host_linux-x86_clang-5484270 -b 9.0.3 clang
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
@@ -19,7 +19,7 @@ export LC_ALL=C
 make O=out ARCH=arm64 RMX2185_defconfig
 
 echo "===+++ Compiling... +++==="
-PATH="$HOME/work/toolchain/clang/clang-r353983c/bin:${PATH}:$HOME/work/toolchain/los-4.9-32/bin:${PATH}:$HOME/work/toolchain/los-4.9-64/bin:${PATH}" \
+PATH="$HOME/work/toolchain/clang/bin:${PATH}:$HOME/work/toolchain/los-4.9-32/bin:${PATH}:$HOME/work/toolchain/los-4.9-64/bin:${PATH}" \
 make O=out \
      ARCH=arm64 \
      CC="ccache clang" \
@@ -27,5 +27,6 @@ make O=out \
      CROSS_COMPILE="$HOME/work/toolchain/los-4.9-64/bin/aarch64-linux-android-" \
      CROSS_COMPILE_ARM32="$HOME/work/toolchain/los-4.9-32/bin/arm-linux-androideabi-" \
      CONFIG_NO_ERROR_ON_MISMATCH=y \
-     -j8
+     -j$(nproc --all)
 
+ls -lR out/arch/arm64/boot
