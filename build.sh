534b4758b12dbaacc1ae51870858a87e57a927e5
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-07 15:26:08 +0530

    Update with new toolchains

--- build.sh
+++ build.sh
@@ -7,18 +7,25 @@ cd ~/work
 git clone --depth=1 https://github.com/HemanthJabalpuri/android_kernel_realme_mt6765 -b android-10.0 kernel
 
 echo "===+++ Downloading toolchain +++==="
-cd ~/work
-git clone --depth=1 https://github.com/techyminati/android_prebuilts_clang_host_linux-x86_clang-5484270 -b 9.0.3 toolchain
+mkdir toolchain
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
 echo "===+++ Building kernel +++==="
 cd ~/work/kernel
 mkdir out
+export LC_ALL=C
 make O=out ARCH=arm64 oppo6765_defconfig
 
 echo "===+++ Compiling... +++==="
-PATH=$HOME/work/toolchain/bin:$PATH \
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=clang \
-                      CROSS_COMPILE=aarch64-linux-gnu- \
-                      CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+PATH="$HOME/work/toolchain/clang/clang-r353983c/bin:${PATH}:$HOME/work/toolchain/los-4.9-32/bin:${PATH}:$HOME/work/toolchain/los-4.9-64/bin:${PATH}" \
+make O=out \
+     ARCH=arm64 \
+     CC="ccache clang" \
+     CLANG_TRIPLE=aarch64-linux-gnu- \
+     CROSS_COMPILE="$HOME/work/toolchain/los-4.9-64/bin/aarch64-linux-android-" \
+     CROSS_COMPILE_ARM32="$HOME/work/toolchain/los-4.9-32/bin/arm-linux-androideabi-" \
+     CONFIG_NO_ERROR_ON_MISMATCH=y \
+     -j8
+
