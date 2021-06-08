22627f9589b5fb24819a511ac5dd7c1eda7a3971
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-08 08:23:30 +0530

    Revert to AOSP Clang

--- build.sh
+++ build.sh
@@ -8,19 +8,22 @@ git clone --depth=1 https://github.com/HemanthJabalpuri/android_kernel_realme_mt
 
 echo "===+++ Downloading toolchain +++==="
 mkdir toolchain && cd toolchain
-git clone --depth=1 https://github.com/kdrag0n/proton-clang
+git clone --depth=1 https://github.com/techyminati/android_prebuilts_clang_host_linux-x86_clang-5484270 -b 9.0.3 clang
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 gcc
 
 echo "===+++ Building kernel +++==="
 cd ~/work/kernel
 mkdir out
 make O=out ARCH=arm64 RMX2185_defconfig
 
-PATH="$HOME/work/toolchain/bin:${PATH}" \
+PATH="$HOME/work/toolchain/clang/bin:$HOME/work/toolchain/gcc64/bin:$HOME/work/toolchain/gcc/bin:${PATH}" \
 make O=out \
      ARCH=arm64 \
      CC=clang \
-     CROSS_COMPILE=aarch64-linux-gnu- \
-     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+     CLANG_TRIPLE=aarch64-linux-gnu- \
+     CROSS_COMPILE=aarch64-linux-android- \
+     CROSS_COMPILE_ARM32=arm-linux-androideabi- \
      -j$(nproc --all)
 
 echo "===+++ Uploading kernel +++==="
