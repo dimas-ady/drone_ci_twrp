0a7237767b96070fdd53d1321bdabe8709f854e1
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-13 07:21:06 +0530

    Full cleanup. This will boot

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ abort() { echo "$1"; exit 1; }
 
 MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0"
 DEVICE=X573
-DT_LINK="https://github.com/HemanthJabalpuri/Hot-S3-Infinix-X573-device -b test3"
+DT_LINK="https://github.com/HemanthJabalpuri/android_device_infinix_X573 -b android-8.0"
 DT_PATH=device/infinix/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
