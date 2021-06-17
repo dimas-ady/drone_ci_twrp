e62e27288d8f379ed76bb16c1bdafd2730f768bb
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-17 12:15:13 +0530

    Fix error

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ git clone --depth=1 https://github.com/HemanthJabalpuri/android_kernel_realme_mt
 echo "===+++ Downloading toolchain +++==="
 mkdir toolchain && cd toolchain
 wget -q -O clang.tar.gz https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/ee5ad7f5229892ff06b476e5b5a11ca1f39bf3a9/clang-r365631c.tar.gz
+#wget -q -O clang.tar.gz https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-10.0.0_r47/clang-r353983c.tar.gz
 mkdir clang && tar xzf clang.tar.gz -C clang
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 gcc64
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 gcc
@@ -27,7 +28,7 @@ make O=out \
      CROSS_COMPILE_ARM32=arm-linux-androideabi- \
      -j$(nproc --all)
 
-echo "===+++ Compiler version +++===
+echo "===+++ Compiler version +++==="
 grep "LINUX_COMPILER" out/include/generated/compile.h
 
 echo "===+++ Uploading kernel +++==="
