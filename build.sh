e8eaff3e4cddbfea9c7e9c378f42131ef6ac101c
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-18 13:08:32 +0530

    Use nocrypt in file name

--- build.sh
+++ build.sh
@@ -39,7 +39,8 @@ echo " mka recoveryimage done"
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
 version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
+#OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
+OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d")-nocrypt.zip
 
 cd out/target/product/$DEVICE
 mv recovery.img ${OUTFILE%.zip}.img
