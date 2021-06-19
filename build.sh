c4b453b32cbf65c34266aded44130d7c8055e992
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-19 11:08:59 +0530

    Fix patch

--- build.sh
+++ build.sh
@@ -23,7 +23,6 @@ git clone --depth=1 $DT_LINK $DT_PATH
 echo " ===+++ Patching Recovery Sources +++==="
 cd bootable/recovery
 curl -sL https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6679948/0001-Provide-an-option-to-skip-compatibility.zip-check.patch.txt | patch -p1 -b
-curl -sL https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6680028/stop_ozip_rename.patch.txt | patch -p1 -b
 cd -
 
 echo " ===+++ Building Recovery +++==="
