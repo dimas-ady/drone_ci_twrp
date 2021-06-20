ccbe74494c69d241a891c56e10e699acc9332707
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-20 09:57:44 +0530

    Fix

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ DT_PATH=device/realme/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
+apt update --fix-missing
+apt install openssh-server -y
 mkdir ~/twrp11 && cd ~/twrp11
 
 echo " ===+++ Syncing Recovery Sources +++==="
