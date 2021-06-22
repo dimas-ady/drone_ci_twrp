58eadf08bd7ae28d8722617d11cfcb7af9f32f21
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-22 21:42:19 +0530

    Build land kernel

--- build.sh
+++ build.sh
@@ -1,61 +1,23 @@
-#!/bin/bash
-# Just a basic script U can improvise lateron asper ur need xD 
+#!/bin/sh
 
-abort() { echo "$1"; exit 1; }
+cd ~
+git clone --depth=1 https://github.com/MiCode/Xiaomi_Kernel_OpenSource -b land-m-oss kernel
+mkdir toolchain && cd toolchain
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 gcc64
+#git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 gcc
 
-MANIFEST="https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0"
-DEVICE=X573
-DT_LINK="https://github.com/HemanthJabalpuri/TWRP_Infinix_X573 -b twrp"
-DT_PATH=device/infinix/$DEVICE
+cd ~/kernel
+export KBUILD_COMPILER_STRING=$($HOME/toolchain/gcc64/bin/aarch64-linux-android-gcc --version | head -n 1)
+export KBUILD_BUILD_USER="HemanthJabalpuri"
+export ARCH=arm64
+export SUBARCH=arm64
 
-echo " ===+++ Setting up Build Environment +++==="
-apt install openssh-server openjdk-8-jdk -y
-apt update --fix-missing
-apt install openssh-server openjdk-8-jdk -y
-git clone https://gitlab.com/OrangeFox/misc/scripts
-cd scripts
-sudo bash setup/android_build_env.sh
-sudo bash setup/install_android_sdk.sh
+export PATH=$HOME/toolchain/gcc64/bin:$PATH
+#export PATH=$HOME/toolchain/gcc64/bin:$HOME/toolchain/gcc:$PATH
+#export CROSS_COMPILE_ARM32=$HOME/toolchain/gcc/bin/arm-linux-androideabi-
+export CROSS_COMPILE=$HOME/toolchain/gcc64/bin/aarch64-linux-android-
 
-echo " ===+++ Syncing Recovery Sources +++==="
-mkdir ~/OrangeFox
-cd ~/OrangeFox
-repo init --depth=1 -u $MANIFEST
-repo sync -j8 --force-sync
-git clone --depth=1 $DT_LINK $DT_PATH
+make msm8937_defconfig
+make -j$(nproc --all)
 
-echo " ===+++ Building Recovery +++==="
-source build/envsetup.sh
-echo " source build/envsetup.sh done"
-
-# Flags
-version=$(cat bootable/recovery/variables.h | grep "define FOX_MAIN_VERSION_STR" | cut -d \" -f2)
-export OF_SCREEN_H=2160
-export OF_ALLOW_DISABLE_NAVBAR=0
-export OF_NO_SPLASH_CHANGE=1
-export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
-
-export OF_MAINTAINER="HemanthJabalpuri"
-export FOX_VERSION="${version}_0"
-export FOX_BUILD_TYPE="test"
-export FOX_REMOVE_AAPT=1
-export FOX_DELETE_INITD_ADDON=1
-export FOX_DELETE_AROMAFM=1
-export FOX_DELETE_MAGISK_ADDON=1
-
-export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export LC_ALL="C"
-lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
-echo " lunch omni_${DEVICE}-eng done"
-mka recoveryimage || abort " mka failed with exit status $?"
-echo " mka recoveryimage done"
-
-# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
-echo " ===+++ Uploading Recovery +++==="
-cd out/target/product/$DEVICE
-ofoxzip="$(ls OrangeFox-*.zip)"
-curl -T $ofoxzip https://oshi.at
-#curl -F "file=@${ofoxzip}" https://file.io
-#curl --upload-file $ofoxzip http://transfer.sh/
+curl -T out/arch/arm64/boot/Image.gz-dtb https://oshi.at
