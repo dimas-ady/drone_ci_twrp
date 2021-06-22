0edd0d79fd025395e5f9195025bf26d3124558bb
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-22 22:42:57 +0530

    Use USE_HOST_LEX=yes
    https://forum.xda-developers.com/t/makefile-error-multiple-target-patterns-stop.3362544/post-66665233

--- build.sh
+++ build.sh
@@ -12,19 +12,20 @@ git clone --depth=1 https://github.com/MiCode/Xiaomi_Kernel_OpenSource -b land-m
 
 echo "===+++ Downloading toolchain +++==="
 mkdir toolchain && cd toolchain
-git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b ndk-r19 gcc64
-git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b ndk-r19 gcc
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 gcc64
+#git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b ndk-r19 gcc
 
 echo "===+++ Building kernel +++==="
 cd ~/kernel
+export USE_HOST_LEX=yes
 #export KBUILD_COMPILER_STRING=$($HOME/toolchain/gcc64/bin/aarch64-linux-android-gcc --version | head -n 1)
 #export KBUILD_BUILD_USER="HemanthJabalpuri"
 export ARCH=arm64
 export SUBARCH=arm64
 
-#export PATH=$HOME/toolchain/gcc64/bin:$PATH
-export PATH=$HOME/toolchain/gcc64/bin:$HOME/toolchain/gcc:$PATH
-export CROSS_COMPILE_ARM32=$HOME/toolchain/gcc/bin/arm-linux-androideabi-
+export PATH=$HOME/toolchain/gcc64/bin:$PATH
+#export PATH=$HOME/toolchain/gcc64/bin:$HOME/toolchain/gcc:$PATH
+#export CROSS_COMPILE_ARM32=$HOME/toolchain/gcc/bin/arm-linux-androideabi-
 export CROSS_COMPILE=$HOME/toolchain/gcc64/bin/aarch64-linux-android-
 
 make msm8937_defconfig || abort " make msm8937_defconfig failed with $?"
