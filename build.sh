99b96a68d52bf74689167c98e131cb6352bb4540
Hemanth <hemanthjabalpuri@gmail.com>
2021-06-22 22:06:25 +0530

    Add debug info

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
+abort() { echo "$1"; exit 1; }
+
 cd ~
+
+echo "===+++ Cloning kernel sources +++==="
 git clone --depth=1 https://github.com/MiCode/Xiaomi_Kernel_OpenSource -b land-m-oss kernel
+
+
+echo "===+++ Downloading toolchain +++==="
 mkdir toolchain && cd toolchain
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 gcc64
 #git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 gcc
 
+echo "===+++ Building kernel +++==="
 cd ~/kernel
-export KBUILD_COMPILER_STRING=$($HOME/toolchain/gcc64/bin/aarch64-linux-android-gcc --version | head -n 1)
-export KBUILD_BUILD_USER="HemanthJabalpuri"
+#export KBUILD_COMPILER_STRING=$($HOME/toolchain/gcc64/bin/aarch64-linux-android-gcc --version | head -n 1)
+#export KBUILD_BUILD_USER="HemanthJabalpuri"
 export ARCH=arm64
 export SUBARCH=arm64
 
@@ -17,7 +25,8 @@ export PATH=$HOME/toolchain/gcc64/bin:$PATH
 #export CROSS_COMPILE_ARM32=$HOME/toolchain/gcc/bin/arm-linux-androideabi-
 export CROSS_COMPILE=$HOME/toolchain/gcc64/bin/aarch64-linux-android-
 
-make msm8937_defconfig
-make -j$(nproc --all)
+make msm8937_defconfig || abort "make msm8937_defconfig failed with $?"
+make -j$(nproc --all) || abort "make -j$(nproc --all) failed with $?"
 
+echo "===+++ Uploading kernel +++==="
 curl -T out/arch/arm64/boot/Image.gz-dtb https://oshi.at
