2a72102c8e856ea29afa3ca143b805ccef0dc6f7
dimas-ady <dimasady123@outlook.co.id>
2021-06-23 08:55:37 +0700

    Change Manifest and build env

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ msg() {
 }
 abort() { echo "$1"; exit 1; }
 
-MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0"
+MANIFEST="https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0"
 DEVICE=X00TD
 DT_LINK="https://github.com/dimas-ady/twrp_device_asus_X00TD.git"
 DT_PATH=device/asus/$DEVICE
@@ -37,20 +37,19 @@ tg_post_build() {
 }
 
 msg "Setting up Build Environment"
-mkdir -p ~/OrangeFox_
-cd ~/OrangeFox
-apt install openssh-server -y
+apt install openssh-server openjdk-8-jdk -y
+apt update --fix-missing
+apt install openssh-server openjdk-8-jdk -y
 git clone https://gitlab.com/OrangeFox/misc/scripts
 cd scripts
 sudo bash setup/android_build_env.sh
 sudo bash setup/install_android_sdk.sh
 
 msg "Syncing Recovery Source"
+mkdir ~/OrangeFox
 cd ~/OrangeFox
-repo init -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0
-repo sync -j$(nproc --all) --force-sync
-cd ~/OrangeFox
-ls
+repo init --depth=1 -u $MANIFEST
+repo sync -j8 --force-sync
 git clone --depth=1 $DT_LINK $DT_PATH
 
 msg "Building Recovery"
@@ -72,7 +71,7 @@ if [ -f "out/target/product/$DEVICE/recovery.img" ]
 then
   msg "Uploading Recovery"
   version=$(cat bootable/recovery/variables.h | grep "define FOX_MAIN_VERSION_STR" | cut -d \" -f2)
-  OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M")
+  OUTFILE=OrangeFox-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M")
 
   cd out/target/product/$DEVICE
 
